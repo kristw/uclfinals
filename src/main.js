331677065a61d519745f6635f0a1d6b32c45769b
Krist Wongsuphasawat <krist.wongz@gmail.com>
2015-06-06 03:11:21 -0700

    working version

--- src/main.js
+++ src/main.js
@@ -42,113 +42,116 @@ angular.module('app', ['d3.promise'])
   })
   .factory('Vis', function(){
     return d3Kit.factory.createChart({
-      margin: {top: 10, left: 10, right: 10, bottom: 10},
-      initialWidth: 800,
+      margin: {top: 140, left: 40, right: 40, bottom: 50},
+      initialWidth: 1100,
       initialHeight: 600,
       minRadius: 5,
       maxRadius: 26
     }, [], function(skeleton){
 
       var options = skeleton.options();
-      var color = d3.scale.category10();
+      // var color = d3.scale.category10();
+      var color = d3.scale.ordinal()
+        .range(['#779ecb', '#c23b22', '#836953', '#77dd77', '#aec6cf', '#cb99c9', '#ffb347', '#b39eb5', '#966FD6', '#f49ac2']);
 
-      var projection = d3.geo.mercator()
-        .scale(1000)
-        // Customize the projection to make the center of Europe become the center of the map
-        .rotate([-10.3167, -48.3839])
-        .translate([skeleton.getInnerWidth() / 2, skeleton.getInnerHeight() / 2]);
-
-      var path = d3.geo.path()
-        .projection(projection);
-
-      var world = Physics();
-      world.add([
-        Physics.behavior('edge-collision-detection', {
-          aabb: Physics.aabb(0,0, skeleton.getInnerWidth(), skeleton.getInnerHeight()),
-          restitution: 0.99,
-          cof: 0.99
-        }),
-        Physics.behavior('body-collision-detection'),
-        Physics.behavior('body-impulse-response'),
-        Physics.behavior('sweep-prune')
-      ]);
-      world.on('step', function(){
-        visualize();
-      });
-
-      skeleton.on('data', function(data){
-        console.log('data', data);
-
-        data.teams.map(function(team){
-          var xy = projection([team.nation.lon, team.nation.lat]);
-          console.log('xy', xy);
-
-          var body = Physics.body('circle', {
-            x: xy[0] + Math.random()*10,
-            y: xy[1] + Math.random()*10,
-            radius: options.maxRadius
-          });
-          team.body = body;
-          body.team = team;
-
-          // var attraction = Physics.behavior('attractor', {
-          //   strength: 2,
-          //   max: 200
-          // })
-          //   .position(xy)
-          //   .applyTo([body]);
-
-          world.add(body);
-          // world.add(attraction);
-        });
-
-        // world.add(bodies);
-        // world.add(attractions);
-
-        Physics.util.ticker.on(function(time, dt){
-          world.step(time);
-        });
-        Physics.util.ticker.start();
-      });
+      skeleton.on('data', visualize);
 
       function visualize(){
         var data = skeleton.data();
+
+        data.teams.sort(function(a,b){
+          return a.nation.name.localeCompare(b.nation.name);
+        })
+        .forEach(function(team, i){
+          team.index = i;
+        });
+
+        skeleton.height(data.teams.length*20 + options.margin.top+options.margin.bottom);
+
+        skeleton.getRootG().append('text')
+          .attr('x', skeleton.getInnerWidth()/2)
+          .attr('y', -70)
+          .classed('title', true)
+          .text('European Cup and UEFA Champions League Finals');
 
         var selection = skeleton.getRootG().selectAll('g.team')
           .data(data.teams, function(d){return d.name;});
 
         var sEnter = selection.enter().append('g')
           .classed('team', true)
-          .attr('transform', function(d){return 'translate('+(d.body.state.pos.x)+','+(d.body.state.pos.y)+')';});
+          .attr('transform', function(d, i){return 'translate('+(150)+','+(i* 20)+')';});
 
         sEnter.append('circle')
           .style('fill', function(d){return color(d.nation.name);})
           .attr('r', options.minRadius);
 
-        // sEnter.append('text')
-        //   .attr('x', 10)
-        //   .attr('y', 20)
-        //   .text(function(d){return d.name;});
+        sEnter.append('text')
+          .attr('x', -10)
+          .attr('y', 4)
+          .style('text-anchor', 'end')
+          .text(function(d){return d.name;});
 
-        selection
-          .attr('transform', function(d){return 'translate('+(d.body.state.pos.x)+','+(d.body.state.pos.y)+')';});
-
-        var edges = skeleton.getRootG().selectAll('line')
-          .data(data.matches, function(d){return d.year;})
-
-        edges.enter().append('line')
+        sEnter.append('line')
           .style('stroke-width', 1)
-          .style('stroke', '#000')
-          .attr('x1', function(d){return d.winners.body.state.pos.x;})
-          .attr('y1', function(d){return d.winners.body.state.pos.y;})
-          .attr('x2', function(d){return d.runnersUp.body.state.pos.x;})
-          .attr('y2', function(d){return d.runnersUp.body.state.pos.y;})
+          .style('stroke', '#777')
+          .style('stroke-dasharray', '2,2')
+          .attr('x1', 4)
+          .attr('x2', skeleton.getInnerWidth()-150);
 
-        edges
-          .attr('x1', function(d){return d.winners.body.state.pos.x;})
-          .attr('y1', function(d){return d.winners.body.state.pos.y;})
-          .attr('x2', function(d){return d.runnersUp.body.state.pos.x;})
-          .attr('y2', function(d){return d.runnersUp.body.state.pos.y;})
+        var edges = skeleton.getRootG().selectAll('g.match')
+          .data(data.matches, function(d){return d.year;});
+
+        var edgesEnter = edges.enter().append('g')
+          .classed('match', true)
+          .attr('transform', function(d){return 'translate('+(yearPos(d))+','+(0)+')';});
+
+        edgesEnter.append('line')
+          .style('stroke-width', 2)
+          // .style('stroke', '#000')
+          .style('stroke', function(d){return color(d.winners.nation.name);})
+          .attr('y1', winnerPos)
+          .attr('y2', runnersUpPos);
+
+        edgesEnter.append('circle')
+          .style('fill', function(d){return color(d.winners.nation.name);})
+          .attr('cy', winnerPos)
+          .attr('r', 4);
+
+        edgesEnter.append('circle')
+          .attr('cy', runnersUpPos)
+          .attr('r', 4)
+          .style('fill', '#fff')
+          .style('stroke', '#222')
+          .style('stroke-width', 1);
+
+        edgesEnter.append('g')
+          .attr('transform', 'rotate(-90)')
+          .append('text')
+          .attr('x', function(d){return 8-Math.min(winnerPos(d), runnersUpPos(d));})
+          .attr('y', 3)
+          .text(function(d){
+            return d.score; //d.year;
+          });
+
+        edgesEnter.append('g')
+          .attr('transform', 'rotate(-90)')
+          .append('text')
+          .attr('x', function(d){return -8-Math.max(winnerPos(d), runnersUpPos(d));})
+          .attr('y', 3)
+          .style('text-anchor', 'end')
+          .text(function(d){return d.year;});
+      }
+
+      function yearPos(d){
+        return 150 + (d.year-1955) * 14 + 20;
+      }
+
+      function winnerPos(d){
+        return d.winners.index * 20;
+      }
+
+      function runnersUpPos(d){
+        return d.runnersUp.index * 20;
       }
 
     });
